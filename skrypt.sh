4ab94c430e6e248cc4b97b96db0c7dbd6eff239d
majfu <ma.furmaniuk@gmail.com>
2024-05-03 18:02:10 +0200

    init task

--- skrypt.sh
+++ skrypt.sh
@@ -21,10 +21,20 @@ if [[ "$1" = "--logs" || "$1" = "-l" ]] && ! [[ "$2" =~ "^[0-9]+$" ]]; then
                 current_date=$(date)
                 echo "log$i.txt, skrypt.sh, $current_date" > log$i.txt
 	done
+<<<<<<< HEAD
 elif [[ "$1" = "--logs" || "$1" = "-l" ]]; then
+=======
+elif [ "$1" = "--logs" ]; then
+>>>>>>> 44ccec59a6fd6940caf49b6f2e58d36c6d90ee05
 	echo "Invalid second argument"
 fi
 
 if [[ "$1" = "--help" || "$1" = "-h" ]]; then
 	echo "type --date to display current date, --logs with optional second numerical argument to create log files, if no second argument is provided 100 log files will be created"
+fi
+
+if [[ "$1" = "--init" || "$1" = "-i" ]];then
+	current_dir=$(pwd)
+	git clone https://github.com/majfu/labo4 $current_dir
+	export PATH="$current_dir:$PATH"
 fi
