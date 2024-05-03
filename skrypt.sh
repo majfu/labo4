cc3f65b6cec39a51809a2bc4b0a5bc4f8a14e17c
majfu <ma.furmaniuk@gmail.com>
2024-05-03 15:02:08 +0200

    new flags task

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ "$1" = "--date" ]
+if [ "$1" = "--date" ] || [ "$1" = "-d" ]
 then
 	current_date=$(date)
 	echo $current_date
 fi
 
-if [ "$1" = "--logs" ] && [ -z "$2" ]
+if [[ "$1" = "--logs" || "$1" = "-l" ]] && [ -z "$2" ]
 then
 	for ((i = 1; i <= 100; i++)); do
 		touch log$i.txt
@@ -15,16 +15,16 @@ then
 fi
 
 
-if [ "$1" = "--logs" ] && ! [[ "$2" =~ "^[0-9]+$" ]]; then
+if [[ "$1" = "--logs" || "$1" = "-l" ]] && ! [[ "$2" =~ "^[0-9]+$" ]]; then
 	for ((i = 1; i <= $2; i++)); do
                 touch log$i.txt
                 current_date=$(date)
                 echo "log$i.txt, skrypt.sh, $current_date" > log$i.txt
 	done
-else
+elif [[ "$1" = "--logs" || "$1" = "-l" ]]; then
 	echo "Invalid second argument"
 fi
 
-if [ "$1" = "--help" ]; then
+if [[ "$1" = "--help" || "$1" = "-h" ]]; then
 	echo "type --date to display current date, --logs with optional second numerical argument to create log files, if no second argument is provided 100 log files will be created"
 fi
