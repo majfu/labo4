f68c1337a78b0595a06d1b860a79bc226f95fba2
majfu <ma.furmaniuk@gmail.com>
2024-05-03 12:29:51 +0200

    logs flag file added

---
+++ skrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" = "--logs" ]
+then
+	for ((i = 1; i <= 100; i++)); do
+		touch log$i.txt
+		current_date=$(date)
+		echo "log$i.txt, skrypt.sh, $current_date" > log$i.txt
+fi
