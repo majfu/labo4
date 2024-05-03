5a72e3746bed709e26767ccb9876ba663ce66c1d
majfu <ma.furmaniuk@gmail.com>
2024-05-03 13:43:14 +0200

    added logNum task

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-if [ "$1" = "--logs" ]
+if [ "$1" = "--logs" ] && [ -z "$2" ]
 then
 	for ((i = 1; i <= 100; i++)); do
 		touch log$i.txt
 		current_date=$(date)
 		echo "log$i.txt, skrypt.sh, $current_date" > log$i.txt
+	done
+fi
+
+
+if [ "$1" = "--logs" ] && ! [[ "$2" =~ "^[0-9]+$" ]]; then
+	for ((i = 1; i <= $2; i++)); do
+                touch log$i.txt
+                current_date=$(date)
+                echo "log$i.txt, skrypt.sh, $current_date" > log$i.txt
+	done
+else
+	echo "Invalid second argument"
 fi
