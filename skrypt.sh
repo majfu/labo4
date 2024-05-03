6bbdb2b5171a3b166f0e06135ba9321975aa85f6
majfu <ma.furmaniuk@gmail.com>
2024-05-03 14:17:30 +0200

    added help task

--- skrypt.sh
+++ skrypt.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
-<<<<<<< HEAD
-if [ "$1" = "--logs" ] && [ -z "$2" ]
-=======
 if [ "$1" = "--date" ]
 then
 	current_date=$(date)
 	echo $current_date
 fi
 
-if [ "$1" = "--logs" ]
->>>>>>> logFilesBranch
+if [ "$1" = "--logs" ] && [ -z "$2" ]
 then
 	for ((i = 1; i <= 100; i++)); do
 		touch log$i.txt
@@ -28,4 +23,8 @@ if [ "$1" = "--logs" ] && ! [[ "$2" =~ "^[0-9]+$" ]]; then
 	done
 else
 	echo "Invalid second argument"
+fi
+
+if [ "$1" = "--help" ]; then
+	echo "type --date to display current date, --logs with optional second numerical argument to create log files, if no second argument is provided 100 log files will be created"
 fi
